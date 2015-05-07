56613cecb3be8a3c8291e373d3cf5f9d585ba86b
Kay Odenthal <Kay.Odenthal@googlemail.com>
2015-05-07 11:58:32 +0200

    sequential version with results in the INPUT array (overriding values)

--- u2/a2/a2.c
+++ u2/a2/a2.c
@@ -1,43 +1,62 @@
 
 #include <stdio.h>
+#include <math.h>
 
+typedef double atype_t;
 
-int sum(int x, int y) {
+atype_t sum(atype_t x, atype_t y) {
 	return x+y;
+}
+
+atype_t balanced_tree(int n1, atype_t values[n1], atype_t (*f)(atype_t x, atype_t y)) {
+	int leafs = log2(n1);
+	int stride, i; 
+	for (stride=leafs/2;stride>0;stride/=2) {
+		for (i=0;i<stride;i++) {
+			values[i] = f(values[i], values[i+stride]);
+		}
+	}
 }
 
 int main()
 {
 	int n = 16;
-	int p = 4;
-	int a[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-	int b[16] = {0};
+	int n1 = 2*n-1;
+	int p = n/log2(n);
+	atype_t a[16] = {0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0};
+	atype_t b[16] = {0};
 	int i, j, stride;
 
 	// Calculate partial sums (1 sum for each processor)
-	for (i=0;i<4;i++) {
-		for (j=0;j<4;j++) {
+	for (i=0;i<p;i++) {
+		for (j=0;j<log2(n);j++) {
 			b[i] += a[i*4+j];
 		}
 	}
-	print_array(0,b,4);
+	print_array(0,b,p);
 
+
+	balanced_tree(n1,b,sum);
 	// Calculate final sum using the binary tree template algorithm
+	/*
 	for (stride=4/2;stride>0;stride/=2) {
 		for (i=0;i<stride;i++) {
 			b[i] = sum(b[i], b[i+stride]);
 		}
 	}
+	*/
 	print_array(0,b,4);
 
 
 	return 0;
 }
 
-void print_array(int threadID, int x[], int size) {
+
+
+void print_array(int threadID, double x[], int size) {
 	printf("--- Array of thread with ID %d ---\n", threadID);
 	int i;
 	for (i=0;i<size;i++) {
-		printf("%d: %d\n",i,x[i]);
+		printf("%d: %.2f\n",i,x[i]);
 	}
 };
