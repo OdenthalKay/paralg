5ceac298aad940bf73a8fb2fb0e92e6dc4c03672
Kay Odenthal <=>
2015-05-21 10:19:35 -0400

    every process is assigned a block that gets sorted independently

--- u3/a2/a2.c
+++ u3/a2/a2.c
@@ -1,65 +1,60 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "libFHBRS/libFHBRS.h"
 
-void printArray(int a[], int length);
 int cmpfunc(const void *a, const void *b);
+int is_sorted(int x[], int length);
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-      printf("Wrong number of arguments. Expecting: <n>\n");
-      return -1;
-    }
-    int n = atoi(argv[1]);
-    double t0, t1;
-    int i;
-    int *values = malloc(n*sizeof(int));
-      
-    // fill array in reverse order     
+  if (argc < 2) {
+    printf("Wrong number of arguments. Expecting: <n>\n");
+    return -1;
+  }
+  int n = atoi(argv[1]);
+  int i, blocksize, rank, num_processors;
+  int *values;
+  int *block;
+  double t0, t1;
+  ////////////////////////////////////////////////////////
+  t0 = gettime();
+  MPI_Init(NULL, NULL);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_processors);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  blocksize = n/num_processors;
+
+  // 1. allocate memory and fill array values in reverse order
+  // 2. split values among all processors
+  if (rank == 0) {
+    values = malloc(n*sizeof(int));
     for (i=0;i<n;i++) {
       values[i] = n-i;
     }
-    
-    t0 = gettime();
 
-   //printf("Before sorting the list is: \n");
-   //printArray(values,n);
-   qsort(values, n, sizeof(int), cmpfunc);
-   //printf("After sorting the list is: \n");
-   //printArray(values,n);
-   
-    // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
+    int *start = &values[blocksize];
+    for (i=1;i<num_processors;i++) {
+      MPI_Send(start, blocksize, MPI_INT, i, 0, MPI_COMM_WORLD);
+      start += blocksize;
+    }
+    qsort(values, blocksize, sizeof(int), cmpfunc);
+    assert(is_sorted(values,blocksize));
+  } else {
+    block = malloc(blocksize*sizeof(int));
+    MPI_Recv(block, blocksize, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    qsort(block, blocksize, sizeof(int), cmpfunc);
+    assert(is_sorted(block, blocksize));
+  }
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-
+  if (rank == 0) {
     t1 = gettime();
     printf("\nCalculation took %f seconds.\n",t1-t0);
-    // Finalize the MPI environment.
-    MPI_Finalize();
-    return 0;
+  }
+  MPI_Finalize();
+  return 0;
 }
 
-////////////////////////////////////////////////////////////////
-void printArray(int a[], int length) {
-   int i;
-   for(i=0;i<length;i++) {
-      printf("%d ", a[i]);
-   }
-   printf("\n");
-}
+///////////////////////////////////////////////////////////
 
 /* 
 returns negative if b > a 
@@ -69,4 +64,14 @@ int cmpfunc(const void *a, const void *b) {
     const int *x = (const int *)a; // casting pointer types 
     const int *y = (const int *)b;
     return *x  - *y;  
-} 
+  } 
+
+int is_sorted(int x[], int length) {
+  int i;
+  for (i=0;i<length-1;i++) {
+    if (x[i+1] < x[i]) {
+      return 0;
+    }
+  }
+  return 1;
+}
