274e4626d4283c02ca020c058730e9894d8cda2c
kay <kay.odenthal@gmx.de>
2015-06-25 18:38:56 +0200

    added jobscript

---
+++ template/job_par.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#PBS -q wr5
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=00:10:00
+#PBS -l vmem=16GB
+#PBS -V 
+
+module load intel-icc/default
+
+# change to submit directory (with executable)
+cd $PBS_O_WORKDIR
+
+echo "N = " $N ":"
+for EXP in 10 11 12 ; do
+	N=$(echo 2^$EXP | bc)
+	for THREADS in 1 2 4 ; do
+		export OMP_NUM_THREADS=$THREADS
+		./seq
+	done
+done
+
+exit
