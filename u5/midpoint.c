2eda1774f1ecc2eb809adc246774c12c26c76dd4
kay <kay.odenthal@gmx.de>
2015-06-30 13:48:07 +0200

    refactoring

---
+++ u5/midpoint.c
@@ -0,0 +1,35 @@
+/*
+* MIDPOINT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+double midpoint(int a, int b, int n) {
+	double h = (double)(b - a) / n;
+	double approx = 0;
+
+	for (int i = 0; i <= (n / 2); i++) {
+		int index = i * 2;
+		double x = a + index * h;
+		approx += sin(x);
+	}
+	return 2*h*approx;
+}
+
+int main(int argc, char const *argv[])
+{
+	double approx, exact;
+	int a = 0;
+	int b = 1000;
+	int n = 100000000;
+
+	printf("--- midpoint: sequential integral ---\n");
+	approx = midpoint(a, b, n);
+	exact  = (-cos(b)) - (-cos(a));
+	printf("midpoint approx: %.5f\n", approx);
+	printf("exact: %.5f\n",  exact);
+	return 0;
+}
--- u5/seq.c
+++ u5/seq.c
@@ -1,23 +1,17 @@
 
+/*
+* TRAPEZOID
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define PI 3.14159265
-
-double midpoint(int a, int b, int n) {
+double trapezoid(int a, int b, int n) {
 	double h = (double)(b - a) / n;
 	double approx = 0;
 
-	for (int i = 0; i <= (n / 2); i++) {
-		int index = i * 2;
-		double x = a + index * h;
-		approx += 2 * h * sin(x);
-	}
 	return approx;
 }
-
-
 
 int main(int argc, char const *argv[])
 {
@@ -26,10 +20,11 @@ int main(int argc, char const *argv[])
 	int b = 1000;
 	int n = 100000000;
 
-	printf("--- midpoint: sequential integral ---\n");
-	approx = midpoint(a, b, n);
+	printf("--- trapezoid: sequential integral ---\n");
+	approx = trapezoid(a, b, n);
 	exact  = (-cos(b)) - (-cos(a));
-	printf("midpoint approx: %.5f\n", approx);
+	printf("trapezoid approx: %.5f\n", approx);
 	printf("exact: %.5f\n",  exact);
 	return 0;
 }
+
